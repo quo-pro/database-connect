d2d12d359ff13853bb6e99dfb4a0918c1c8fec72
Israel Ayokunnu <38165966+easyadin@users.noreply.github.com>
2024-04-21 01:46:59 +0400

    update schema

--- dist/src/collections/friend/schema.js
+++ dist/src/collections/friend/schema.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.FriendModel = void 0;
-const commons_1 = require("@quo-pro/commons");
 const mongoose_1 = require("mongoose");
 const Model = new mongoose_1.Schema({
     user: {
@@ -13,11 +12,6 @@ const Model = new mongoose_1.Schema({
         type: mongoose_1.Schema.Types.ObjectId,
         required: true,
         ref: 'User'
-    },
-    status: {
-        type: mongoose_1.Schema.Types.String,
-        required: true,
-        enum: commons_1.FRIEND_STATUS_LIST
     },
 }, { timestamps: true });
 exports.FriendModel = (0, mongoose_1.model)("Friend", Model);
--- src/collections/friend/schema.ts
+++ src/collections/friend/schema.ts
@@ -1,4 +1,4 @@
-import { FRIEND_STATUS_LIST, IFriend, } from "@quo-pro/commons";
+import { IFriend, } from "@quo-pro/commons";
 import { Schema, model } from "mongoose";
 
 const Model = new Schema<IFriend>(
@@ -12,11 +12,6 @@ const Model = new Schema<IFriend>(
       type: Schema.Types.ObjectId,
       required: true,
       ref: 'User'
-    },
-    status: {
-      type: Schema.Types.String,
-      required: true,
-      enum: FRIEND_STATUS_LIST
     },
   },
   { timestamps: true }
